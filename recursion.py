9954f3f0651ebb805e40d939e2e1d5b5fbcc0958
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2023-07-22 11:42:43 +0530

    first commit

---
+++ recursion.py
@@ -0,0 +1,20 @@
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        self = Solution()
+        #Your code here
+        if(D==0):
+            return A
+        temp = A.pop(0)
+        A.append(temp)
+        return self.rotateArr(A,D-1,N)
+        
+N = int(input())
+arr = list()
+for i in range(N):
+     x = int(input())
+     arr.append(x)
+print("Enter the step")
+D = int(input())
+ans = Solution().rotateArr(arr,D,N)
+print(ans)
