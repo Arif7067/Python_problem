6b51a16981f2dd1a173f0b3b0c62292a4c52fc2f
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2023-07-31 19:09:25 +0530

    DSA

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+    # ll.inser_values(["bananas","apples","mangos","grapes"])
+    # ll.printing()
+    # ll.remove_at(2)
+    # ll.printing()
+    # ll.insert_at(0,"potatoes")
+    # ll.printing()
+    # ll.insert_at(3,"Kerala")
+    # ll.printing()
