b4981c96007714d05d18bb4e72d40bc719c765de
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2023-08-16 22:38:38 +0530

    second commit

--- middle_elment_in_linkedlist.py
+++ middle_elment_in_linkedlist.py
@@ -1,41 +1,25 @@
+# Given a singly linked list of N nodes.
+# The task is to find the middle of the linked list. For example, if the linked list is
+# 1-> 2->3->4->5, then the middle node of the list is 3.
+# If there are two middle nodes(in case, when N is even), print the second middle element.
+# For example, if the linked list given is 1->2->3->4->5->6, then the middle node of the list is 4.
+
+
+
 class node:
-    def __init__(self,data=None,next=None):
+    def __init__(data):
         self.data = data
         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-    def insert_at_end(self,data):
-        if self.head is None:
-            self.head = node(data,None)
-            return
-
 class Solution:
-    #  Should return data of middle node. If linked list is empty, then  -1
     def findMid(self, head):
-        # Code here
-        # return the value stored in the middle node
-        count = 1
-        while head!= None:
+        count = 0
+        t=head
+        while t!= None:
             count+=1
-            print(head)
-            head = head.next
-        return count
-        # return n
-        c=0
-        while head.next!= None:
-            c+=1
-            if c==n:
-                return head.data
-            else :
-                head = head.next
-
-ll = LinkedList()
-ll.insert_at_end(8)
-ll.insert_at_end(9)
-ll.insert_at_end(7)
-ll.insert_at_end(10)
-
-s = Solution()
-print(s.findMid(ll.head))
+            t = t.next
+        n=count//2
+        while n!= 0:
+            head= head.next
+            n-=1
+        return head.data
