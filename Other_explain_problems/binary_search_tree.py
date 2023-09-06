c607891ee33510af001995cdb35976341f8a3ca5
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2023-09-06 19:40:53 +0530

    sixth day

---
+++ Other_explain_problems/binary_search_tree.py
@@ -0,0 +1,112 @@
+class Binary_search_Tree:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def add_elements(self, data):
+        if data == self.data:
+            return
+        if data <self.data:
+            if self.left:
+                self.left.add_elements(data)
+            else:
+                self.left = Binary_search_Tree(data)
+        if data > self.data:
+            if self.right:
+                self.right.add_elements(data)
+            else:
+                self.right = Binary_search_Tree(data)
+
+    def inorder_elements(self):
+        elements = []
+        if self.left:
+            elements += self.left.inorder_elements()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements+=self.right.inorder_elements()
+
+        return elements
+    
+    def preorder_elements(self):
+        elements = []
+        elements.append(self.data)
+        if self.left:
+            elements += self.left.preorder_elements()     
+        if self.right:
+            elements+=self.right.preorder_elements()
+
+        return elements
+    
+    def postorder_elements(self):
+        elements = []        
+        if self.left:
+            elements += self.left.postorder_elements()     
+        if self.right:
+            elements+=self.right.postorder_elements()
+        elements.append(self.data)
+        return elements
+    
+    def search(self,val):
+        if val ==self.data:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+            
+        if val > self.data :
+            if self.right:
+                return self.right.search(val)
+            else :
+                return False
+    
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        return self.data
+    
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        return self.data
+    
+    def calculate_sum(self):
+        sum=0
+        if self.left:
+            sum+=self.left.calculate_sum()
+        sum += self.data
+        if self.right:
+            sum+=self.right.calculate_sum()
+        return sum
+    
+
+def build_tree(elements):
+    root = Binary_search_Tree(elements[0])
+    for i in range(1,len(elements)):
+        root.add_elements(elements[i])
+    return root
+
+
+if __name__ == "__main__":
+
+    # countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+    # country_tree = build_tree(countries)
+
+    # print(country_tree.inorder_elements())
+    # print("UK is in the list? ", country_tree.search("UK"))
+    # print("Sweden is in the list? ", country_tree.search("Sweden"))
+
+    numbers = [15,12,27,88,20,14,7,23]
+    root = build_tree(numbers)
+    print(root.inorder_elements())
+    print(root.search(0))
+    print(root.find_min())
+    print(root.find_max())
+    print(root.calculate_sum())
+    print(root.preorder_elements())
+    print(root.postorder_elements())
+
